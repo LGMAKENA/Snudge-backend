893e9f877befa01297b8fda56cafd97a2a12c02f
lgmakena <linetmakenag@gmail.com>
2023-10-23 21:19:34 +0300

    wishlist

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,2 +1,23 @@
 class WishlistsController < ApplicationController
+    def create
+        wishlist= Wishlist.create!(wishlist_params)
+       if wishlist
+        render json: wishlist, status: :created
+       else
+        render json: {message: "unable to create wishlist"}, status: :internal_server_error
+
+       end
+    end
+    
+    def destroy
+        @wishlist=Wishlist.find(params[:id])
+        if @wishlist
+            @wishlist.destroy
+            head :no_content
+        else
+            render json: {message: "wishlist not found"}, status: :not_found 
+        end
+       
+
+    end
 end
