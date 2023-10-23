cd59725c3d32f036c06544b0720aa8db292fd71f
lgmakena <linetmakenag@gmail.com>
2023-10-23 21:02:53 +0300

    comments parent update

--- app/controllers/comment_parents_controller.rb
+++ app/controllers/comment_parents_controller.rb
@@ -1,4 +1,6 @@
 class CommentParentsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    before_action :set_content, only: [:show, :update, :destroy]
     def create
         comment= CommentParent.create!(comment_params)
        if comment
@@ -8,6 +10,15 @@ class CommentParentsController < ApplicationController
 
        end
     end
+    def update
+        if @comment.update(comment_params)
+            render json: @comment 
+        end
+    end
+    def destroy
+        @comment.destroy
+        head :no_content
+    end
     private 
     def comment_params
         params.permit(:user_id,:comment).merge(content_id: params[:id])
@@ -16,4 +27,8 @@ class CommentParentsController < ApplicationController
     def set_content
         @comment = CommentParent.find(params[:id])
     end
+    def record_not_found
+        render json: {error: "Comment not found"},status: :not_found
+    end
+
 end
