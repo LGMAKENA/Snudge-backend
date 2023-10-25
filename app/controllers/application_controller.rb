6e1b6fe2688679c2c84342a33fc45f229e515eb9
samuel <samuelkelvinmwangi@gmail.com>
2023-10-25 11:25:44 +0300

    adding authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+
+    before_action :authorized
     def check_role
         if session[:user_id].nil?
             render json: { message: 'You are not logged in'}, status: :unauthorized
@@ -12,6 +14,11 @@ class ApplicationController < ActionController::API
         end
         
         # render json: {user: session[:user_id]}
+    end
+
+    def authorized
+        return render json:{error: "Not Authorised"}, status: :unauthorized
+        unless session.include? :user_id
     end
 
     def create
