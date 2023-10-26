b770ffbe2c45932418d655506d6d187e38a3ceda
LGMAKENA <127510547+LGMAKENA@users.noreply.github.com>
2023-10-26 16:47:32 +0300

    update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    skip_before_action :authenticate, only: [:login]
+  
     rescue_from ActiveRecord::RecordInvalid,with: :validation
 
     def login
